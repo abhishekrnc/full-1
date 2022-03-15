f9dec9c4b47a10e6fcb47bc236cfa2fc614cfb69
Abhishek <abhishektupudana@gmail.com>
2022-03-15 09:34:22 +0530

    routing uodate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegistraionComponent } from './registraion/registraion.component';
 
-const routes: Routes = [{path:'', component: HomeComponent}];
+const routes: Routes = [{path:'', component: HomeComponent}, {path:'signup', component: RegistraionComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
