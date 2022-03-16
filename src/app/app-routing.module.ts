cfd26eea08497325f6d92385715ab0c2e62b9975
Abhishek <abhishektupudana@gmail.com>
2022-03-16 12:51:15 +0530

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForgotComponent } from './forgot/forgot.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegistraionComponent } from './registraion/registraion.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [{path:'', component: HomeComponent}, {path:'signup', component: RegistraionComponent}, {path:'signin', component:SignInComponent}, {path:'forgot', component: ForgotComponent}];
+const routes: Routes = [{path:'', component: HomeComponent}, {path:'signup', component: RegistraionComponent}, {path:'signin', component:SignInComponent}, {path:'forgot', component: ForgotComponent}, {path:'profile', component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
